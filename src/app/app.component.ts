a396ac7b433ab29541cb8a6d09fe20e6c34e4b03
mikael.tallbo <mikael.tallbo@gmail.com>
2024-05-25 22:48:10 +0200

    new msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Jompa e downbad SCHNEEHEEE</div>`,
 })
 export class AppComponent {
   value = 'World';
