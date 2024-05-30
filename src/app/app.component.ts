54fc30d74d984240ccc93d0c8bb07ec73208ec51
Mikael Tallbo <mikael.tallbo@gmail.com>
2024-05-30 14:13:44 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Jompa e downbad SCHNEEHEEE, matte e downbad för AH</div>`,
+  template: `<div>Jompa e downbad SCHNEEHEEE, matte e downbad för AH, patte är kär i soffan</div>`,
 })
 export class AppComponent {
   value = 'World';
